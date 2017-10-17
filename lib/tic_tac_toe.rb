1b90792b1542ca3bf5f819365521b8b08d6ec566
Amanda Ackerman <ackerm44@gmail.com>
2017-10-17 15:28:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,6 +34,24 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |cell|
+    if cell == "X" || cell == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end  
+
 def play(board)
   counter = 0
   until counter == 9
